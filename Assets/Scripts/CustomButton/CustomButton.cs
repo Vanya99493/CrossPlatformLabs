6b908826d2d2d131905e807a0be93b626334f56d
IvanZadorozhnii <vanyazadorozhnii99493@gmail.com>
2025-11-21 12:49:27 +0200

    feat: add custom button

---
+++ Assets/Scripts/CustomButton/CustomButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace CrossPlatform.CustomButtonScripts
+{
+	public class CustomButton : MonoBehaviour
+	{
+		[SerializeField]
+		private Color _normalColor;
+
+		[SerializeField]
+		private Color _hoverColor;
+
+		[SerializeField]
+		private Color _pressColor;
+		
+		private CustomButtonDraw _buttonDraw;
+
+		private void Start()
+		{
+			Rect rect = new Rect(810, 500, 300, 80);
+			_buttonDraw = new CustomButtonDraw(rect, _normalColor, _hoverColor, _pressColor);
+
+			_buttonDraw.OnHover += () => Debug.Log("Hover!");
+			_buttonDraw.OnPressed += () => Debug.Log("Pressed!");
+			_buttonDraw.OnReleased += () => Debug.Log("Released!");
+		}
+
+		private void OnGUI()
+		{
+			_buttonDraw.Draw();
+		}
+	}
+}
